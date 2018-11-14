945ec123905cf6968dfec14d601409ea55967dae
Marko Mikulicic <mkm@bitnami.com>
2018-11-14 18:43:49 +0100

    Cleanup code

--- cmd/zeroed/cmd/edit_common.go
+++ cmd/zeroed/cmd/edit_common.go
@@ -1,13 +1,6 @@
 package cmd
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"time"
-
-	"github.com/bitnami-labs/zerozone/pkg/model"
-	shell "github.com/ipfs/go-ipfs-api"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -31,45 +24,4 @@ func registerEditFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&recordData, "data", "d", "", "record data")
 
 	viper.BindPFlag(fileCfg, cmd.Flags().Lookup(fileCfg))
-}
-
-func openZone(fileName string) (zone *model.Zone, save func() error, err error) {
-	save = func() error {
-		f, err := os.Create(fileName)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-		enc := json.NewEncoder(f)
-		enc.SetIndent("", "  ")
-
-		zone.Generation++
-		if err := enc.Encode(zone); err != nil {
-			return err
-		}
-
-		sh := shell.NewShell(viper.GetString(apiAddrCfg))
-		f.Seek(0, 0)
-		hash, err := sh.Add(f)
-		if err != nil {
-			return err
-		}
-		pubkey := viper.GetString(pubKeyCfg)
-		fmt.Fprintf(os.Stderr, "Publishing to IPNS\n")
-		_, err = sh.PublishWithDetails(hash, pubkey, 7*24*time.Hour, 30*time.Second, false)
-		return err
-	}
-
-	f, err := os.Open(fileName)
-	if os.IsNotExist(err) {
-		return &model.Zone{}, save, nil
-	}
-	if err != nil {
-		return nil, nil, err
-	}
-	defer f.Close()
-	if err := json.NewDecoder(f).Decode(&zone); err != nil {
-		return nil, nil, err
-	}
-	return zone, save, nil
 }
--- cmd/zeroed/cmd/zone_common.go
+++ cmd/zeroed/cmd/zone_common.go
@@ -2,19 +2,75 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"time"
 
+	"github.com/bitnami-labs/zerozone/pkg/model"
 	"github.com/ipfs/go-cid"
 	shell "github.com/ipfs/go-ipfs-api"
 	"github.com/multiformats/go-multibase"
 	"github.com/spf13/viper"
 )
 
+// KeyListResult is the result of the key/list IPFS API method.
+// It's here because it's not exported by go-ipfs-api.
 type KeyListResult struct {
 	Keys []struct {
 		Name string
 		Id   string
 	}
+}
+
+func openZone(fileName string) (zone *model.Zone, save func() error, err error) {
+	save = func() error {
+		return updateZone(fileName, zone)
+	}
+
+	f, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return &model.Zone{}, save, nil
+	}
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+	if err := json.NewDecoder(f).Decode(&zone); err != nil {
+		return nil, nil, err
+	}
+	return zone, save, nil
+}
+
+func updateZone(fileName string, zone *model.Zone) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+
+	zone.Generation++
+	if err := enc.Encode(zone); err != nil {
+		return err
+	}
+
+	f.Seek(0, 0)
+	return publishZone(f)
+}
+
+func publishZone(r io.Reader) error {
+	sh := shell.NewShell(viper.GetString(apiAddrCfg))
+	hash, err := sh.Add(r)
+	if err != nil {
+		return err
+	}
+	pubkey := viper.GetString(pubKeyCfg)
+	fmt.Fprintf(os.Stderr, "Publishing /ipfs/%s to IPNS\n", hash)
+	_, err = sh.PublishWithDetails(hash, pubkey, 7*24*time.Hour, 30*time.Second, false)
+	return err
 }
 
 func getZoneName() (string, error) {
